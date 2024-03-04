41d291c6b78c6498eb85e84156a1ca79617a83c3
mskumar07 <mskumar4510@gmail.com>
2024-03-04 12:49:56 +0530

    now change

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
+const {Sequelize,DataTypes} = require('sequelize');
 
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+
+const fast = sequelize.define('fast', {
+    email: Sequelize.STRING,
+})
+
+sequelize.sync()
+  .then(() => {
+    console.log('Database and tables created!');
+  })
+  .catch(err => {
+    console.error('Error creating database and tables:', err);
+  });
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
@@ -10,6 +29,11 @@ app.all('/', (req, res) => {
 app.all('/get_data', (req, res) => {
     console.log("get-data api")
     res.send('hii how are you')
+})
+
+app.all('/db_get_data', async (req, res) => {
+    const Data = await fast.findAll()
+    res.send({title:Data})
 })
 
 app.listen(process.env.PORT || 3000)
