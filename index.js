b748f64551ae91a1935453032d2e57ae9105bf82
mskumar07 <mskumar4510@gmail.com>
2024-03-04 12:33:38 +0530

    now change

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Hello world!')
 })
+
+app.all('/get_data', (req, res) => {
+    console.log("get-data api")
+    res.send('hii how are you')
+})
+
 app.listen(process.env.PORT || 3000)
